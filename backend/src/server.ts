c1479f7e52e2efa8b86d0f7a4762e093cb4d23ab
RAWAT DIKSHA <ite001451@athuman.com>
2023-06-01 14:22:17 +0900

    changes made

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -26,10 +26,11 @@ const mongoDBstore = new MongoDBStore({
 
 app.use(
   cors({
-    allowedHeaders: ['Content-Type','Authorization'],
-    origin: "http://localhost:3000",
+    allowedHeaders: ['Content-Type','Authorization',],
+    origin: "https://rawatdiksha10.github.io",
+    // origin: "http://localhost:3000",
     methods: ["GET","POST","PUT","DELETE","OPTIONS","HEAD"],
-    credentials: true
+    credentials: true,
   })
 );
 
